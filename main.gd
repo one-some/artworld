a906071bb53f5df5280a36fd115c42059c56c2d4
one-some <onesomegit@outlook.com>
2025-02-02 03:08:55 -0600

    iMORE

--- main.gd
+++ main.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color.BLACK)
-	$CanvasModulate.color = Color("0a0a0a")
+	# $CanvasModulate.color = Color("0a0a0a")
+	$CanvasModulate.color = Color.BLACK
